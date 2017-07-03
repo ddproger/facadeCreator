2df4ca0a9e37f62c24d2c16d8e1ba6ddb8c86871
d.gerasymiuk <d.gerasymiuk@gmail.com>
2017-07-03 17:38:02 +0300

    add temporary interfacce to work with kitchen draw API

---
+++ FacadeCreatorApi/models/ApplyingToKD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeCreatorApi.models
+{
+    public interface ApplyingToKD
+    {
+        void apply(KdSdkApi kdApi);
+    }
+}
